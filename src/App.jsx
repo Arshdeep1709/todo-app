872cec3e5144d2f24e5f4ae10e4645f1800ac392
Arshdeep Singh <arshdeepchhabra1712@gmail.com>
2026-01-28 21:44:22 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ function App() {
   const [todo, settodo] = useState('')
   const [todos, settodos] = useState([])
 
+  useEffect(() => {
+    let todoString = localStorage.getItem('todos')
+    if(todoString){
+    let todos = JSON.parse(localStorage.getItem('todos'))
+    settodos(todos)
+    }
+  }, [])
+
+  const saveLocalStorage = ()=>{
+    localStorage.setItem('todos',JSON.stringify(todos))
+  }
+
   const edit = (e,id)=>{
     let t = todos.filter(item=>item.id === id)
     settodo(t[0].todo)
@@ -12,20 +24,27 @@ function App() {
       return item.id!==id
     })
     settodos(newTodos)
+    saveLocalStorage()
+    localStorage.setItem('todos', JSON.stringify(newTodos))
   }
   const dlt = (e,id)=>{
     let newTodos = todos.filter(item=>{
       return item.id!==id
     })
     settodos(newTodos)
+    saveLocalStorage()
+    localStorage.setItem('todos', JSON.stringify(newTodos))
   }
+
   const add = ()=>{
     settodos([...todos, {id:uuidv4(), todo, isCompleted : false}])
     settodo("")
+    saveLocalStorage()
   }
   const change = (e)=>{
     settodo(e.target.value)
   }
+  
 
   return (
     <>
@@ -34,7 +53,7 @@ function App() {
       <div className="addTodo">
         <h2>Add a Todo</h2>
         <input onChange={change} value={todo} type="text" />
-        <button onClick={add}>Add</button>
+        <button onClick={add} disabled={todo.length <= 3}>Add</button>
         <h2>Your Todos</h2>
       </div>
       <div className="todos">
