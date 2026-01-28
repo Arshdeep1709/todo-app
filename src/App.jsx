f9fd1a4758335569d100bbb2208e20730a56b39b
Arshdeep Singh <arshdeepchhabra1712@gmail.com>
2026-01-28 20:14:50 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,19 @@ function App() {
   const [todo, settodo] = useState('')
   const [todos, settodos] = useState([])
 
-  const edit = ()=>{
-
+  const edit = (e,id)=>{
+    let t = todos.filter(item=>item.id === id)
+    settodo(t[0].todo)
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    })
+    settodos(newTodos)
   }
-  const dlt = ()=>{
-    
+  const dlt = (e,id)=>{
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    })
+    settodos(newTodos)
   }
   const add = ()=>{
     settodos([...todos, {id:uuidv4(), todo, isCompleted : false}])
@@ -30,14 +38,15 @@ function App() {
         <h2>Your Todos</h2>
       </div>
       <div className="todos">
+        {todos.length === 0 && <div className='ntd'> Nothing to display</div>}
         {todos.map(item=>{
         return <div key={item.id} className="todo">
           <div className="text">
             {item.todo}
           </div>
           <div className="buttons">
-            <button onClick={edit}>Edit</button>
-            <button onClick={dlt}>Delete</button>
+            <button onClick={(e)=>{edit(e,item.id)}}>Edit</button>
+            <button onClick={(e)=>{dlt(e,item.id)}}>Delete</button>
           </div>
         </div>
         })}
